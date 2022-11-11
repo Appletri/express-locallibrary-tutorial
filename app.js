8b58689aa3772057f131210b057a2054a7031952
appletri <tri_duc_lam@hotmail.com>
2022-11-10 17:39:21 -0700

    added files and changes required for deployment

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
-
+app.use(helmet());
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://Appletri:mongodb@cluster0.wqi7gxe.mongodb.net/local_library?retryWrites=true&w=majority&ssl=true";
+
+const dev_db_url = "mongodb+srv://Appletri:mongodb@cluster0.wqi7gxe.mongodb.net/local_library?retryWrites=true&w=majority&ssl=true";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -25,6 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
